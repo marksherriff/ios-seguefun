d1cda75bdc12774c38865fb50acc24111ee1e423
Mark Sherriff <sherriff@virginia.edu>
2018-02-14 11:07:48 -0500

    Initial Commit

---
+++ SegueFunS18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegueFunS18
+//
+//  Created by sherriff on 2/14/18.
+//  Copyright © 2018 Mark Sherriff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
